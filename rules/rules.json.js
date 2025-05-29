acd01d32074d675ac3e55d89fb7b6fa0e847eacd
wzb <waimaoserver@163.com>
2025-05-29 17:39:31 +0800

    01

---
+++ rules/rules.json.js
@@ -0,0 +1,108 @@
+{
+    "gfwlist": {
+    "name": "gfwlist.conf",
+        "date": "2025-03-17 18:49",
+        "md5": "2223afa879ded68779f60b4c15b9b6b8",
+        "count": "7087"
+},
+    "gfwlist_txt": {
+    "name": "gfwlist.txt",
+        "date": "2025-03-17 18:49",
+        "md5": "7b6facde30ee05be3f750dfde548b6ca",
+        "count": "7087"
+},
+    "chnroute_maxmind": {
+    "name": "chnroute_maxmind.txt",
+        "date": "2025-03-17 18:49",
+        "md5": "41302df69ece45fe11d3e9ddcad2c47e",
+        "count": "7212",
+        "count_ip": "345532148",
+        "source": "maxmind/geolite2",
+        "url": "https://github.com/firehol/blocklist-ipsets/blob/master/geolite2_country/country_cn.netset"
+},
+    "chnroute_ipip": {
+    "name": "chnroute_ipip.txt",
+        "date": "2025-03-17 21:00",
+        "md5": "eb10b35aefa24dd4fdca6a8e3ef025c7",
+        "count": "7449",
+        "count_ip": "354155170",
+        "source": "ipip.net",
+        "url": "https://github.com/firehol/blocklist-ipsets/blob/master/ipip_country/ipip_country_cn.netset"
+},
+    "chnroute_17mon": {
+    "name": "chnroute_17mon.txt",
+        "date": "2025-03-17 18:49",
+        "md5": "132a308ee9cd2631071c3754b0342ec5",
+        "count": "7456",
+        "count_ip": "354070947",
+        "source": "17mon/ipip",
+        "url": "https://raw.githubusercontent.com/17mon/china_ip_list/refs/heads/master/china_ip_list.txt"
+},
+    "chnroute_misakaio": {
+    "name": "chnroute_misakaio.txt",
+        "date": "2025-03-17 18:49",
+        "md5": "9da308188ad95f344b3836769d5eb72f",
+        "count": "4182",
+        "count_ip": "282357504",
+        "source": "misakaio",
+        "url": "https://github.com/misakaio/chnroutes2/blob/master/chnroutes.txt"
+},
+    "chnroute_cnisp": {
+    "name": "chnroute_cnisp.txt",
+        "date": "2025-03-17 21:12",
+        "md5": "132a308ee9cd2631071c3754b0342ec5",
+        "count": "7456",
+        "count_ip": "354070947",
+        "source": "cnisp",
+        "url": "https://github.com/gaoyifan/china-operator-ip"
+},
+    "chnroute_apnic": {
+    "name": "chnroute_apnic.txt",
+        "date": "2025-03-17 21:00",
+        "md5": "226d16bc450e9b2f3e80f2667e06565b",
+        "count": "8674",
+        "count_ip": "343144192",
+        "source": "apnic",
+        "url": "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
+},
+    "chnroute": {
+    "name": "chnroute.txt",
+        "date": "2025-03-17 21:12",
+        "md5": "af2530ddf6045156c31d3e63eb236b6e",
+        "count": "8333",
+        "count_ip": "358874217",
+        "source": "fancyss",
+        "url": "https://github.com/hq450/fancyss/tree/3.0/rules"
+},
+    "cdn_china": {
+    "name": "cdn.txt",
+        "date": "2025-03-17 20:56",
+        "md5": "25f0d64f218b9a0018ab4d2db0e8c02c",
+        "count": "96806"
+},
+    "chnlist_txt": {
+    "name": "chnlist.txt",
+        "date": "2025-03-17 20:56",
+        "md5": "25f0d64f218b9a0018ab4d2db0e8c02c",
+        "count": "96806",
+        "note": "same as cdn.txt with different name"
+},
+    "apple_china": {
+    "name": "apple_china.txt",
+        "date": "2025-03-17 18:49",
+        "md5": "5d07e6bb676623b8dd204783362afd8c",
+        "count": "166"
+},
+    "google_china": {
+    "name": "google_china.txt",
+        "date": "2025-03-17 20:58",
+        "md5": "1c442280eb75ca2e63998680fdc65c4c",
+        "count": "141"
+},
+    "cdn_test": {
+    "name": "cdn_test.txt",
+        "date": "2025-03-17 18:49",
+        "md5": "84b034525bca6b60fbffb68dc8fbb9cd",
+        "count": "87"
+}
+}
